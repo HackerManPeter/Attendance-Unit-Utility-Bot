53ab6f3aec9e7a3791d8197bb5fa037beb6b8ca8
Ebueku Uyiosa Peter <pebueku@gmail.com>
2022-02-21 13:03:56 +0100

    Added poll forwarding and refactored code

--- helper.py
+++ helper.py
@@ -1,1 +1,44 @@
-polls_result_text = f'Members Present:{po}'
+import os
+import telebot
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.environ["TOKEN"]
+
+bot = telebot.TeleBot(TOKEN)
+
+# GS = os.environ["GS"]
+AGS = os.environ["AGS"]
+
+
+def send_polls(message):
+    poll_question = "Did you attend Communion Service?"
+    options = ["Yes", "No"]
+    poll_response = bot.send_poll(
+        chat_id=message.chat.id,
+        question=poll_question,
+        options=options,
+    )
+
+    bot.forward_message(
+        chat_id=AGS, from_chat_id=poll_response.chat.id, message_id=poll_response.id
+    )
+
+
+def get_valid_ids(message):
+    chat_id = message.chat.id
+    chat_administrators = bot.get_chat_administrators(chat_id)
+    valid_ids = [admin.user.id for admin in chat_administrators]
+    return valid_ids + [AGS]
+
+
+# def invalid_message(message):
+#     allowed_ids = get_valid_ids(message)
+
+#     if message.from_user.id not in allowed_ids:
+#         bot.reply_to(
+#             message,
+#             f"Sorry {message.from_user.first_name}, you are not authorised to send this command",
+#         )
+#         return False
+#     return True
--- main.py
+++ main.py
@@ -2,62 +2,52 @@ import os
 import time
 from threading import Thread
 
-from helper import polls_result_text
-import schedule
+from helper import send_polls, get_valid_ids
+
+from schedule import every, run_pending, clear
 import telebot
 from dotenv import load_dotenv
 
 load_dotenv()
-TOKEN = os.environ['TOKEN']
-FILE_PATH = int(os.environ['FILE_PATH'])
+TOKEN = os.environ["TOKEN"]
 
 bot = telebot.TeleBot(TOKEN)
-
-GS = os.environ['GS']
-AGS = os.environ['AGS']
 
 
 def schedule_checker():
     while True:
-        schedule.run_pending()
+        run_pending()
         time.sleep(1)
 
 
-def get_valid_ids(message):
-    chat_id = message.chat.id
-    chat_administrators = bot.get_chat_administrators(chat_id)
-
-    return [admin.user.id for admin in chat_administrators]
-
-
-@bot.message_handler(commands=['send_polls'])
+@bot.message_handler(commands=["send_polls"])
 def handle_polls(message):
     allowed_ids = get_valid_ids(message)
-    allowed_ids += [GS, AGS]
+    if message.from_user.id not in allowed_ids:
+        bot.reply_to(
+            message,
+            f"Sorry {message.from_user.first_name}, you are not authorised to send this command",
+        )
+        return
 
-    if message.from_user.id in allowed_ids:
+    bot.reply_to(message, "Polls have been scheduled")
 
-        poll_question = 'Did you attend Communion Service?'
-        options = ['Yes', 'No']
-        schedule.every().Thursday.at('10:00').do(
-            bot.send_poll, chat_id=message.chat.id, question=poll_question, options=options)
-
-        bot.reply_to(message, 'Polls have been scheduled')
-
-    else:
-        bot.reply_to(message, 'Sorry you can\'t send this command')
-
-        schedule.every().Friday.at('21:00').do(bot.send_message, chat_id=[AGS, GS], f)
-
-@bot.message.handler(commands=['stop_polls'])
-def stop_polls(message):
+    # every().thursday.at("9:00").do(send_polls, message)
+    every(30).seconds.do(send_polls, message).tag("Polling")
 
 
-@bot.message_handler(commands=['stop_polls'])
+@bot.message_handler(commands=["stop_polls"])
 def stop_polls(message):
     allowed_ids = get_valid_ids(message)
-    if message.from_user.id in allowed_ids or message.from_user.id == AGS:
-        schedule.cancel_job()
+    if message.from_user.id not in allowed_ids:
+        bot.reply_to(
+            message,
+            f"Sorry {message.from_user.first_name}, you are not authorised to send this command",
+        )
+        return
+
+    clear("Polling")
+    bot.reply_to(message, "Polls have been stopped")
 
 
 Thread(target=schedule_checker, daemon=True).start()
